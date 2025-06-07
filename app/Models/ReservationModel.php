771e52855589b6ee95deada5422f2c473c77825e
Gilberto <gilbertonerigodoy@gmail.com>
2025-06-07 11:53:25 -0300

    Trabalhando com model parte final - Rerservas

--- app/Models/ReservationModel.php
+++ app/Models/ReservationModel.php
@@ -42,11 +42,69 @@ class ReservationModel extends AppModel
        
     }
 
-
-    protected function relateData(object &$area, array $contains = []): void
+    public function all(): array 
     {
-        if(in_array('reservations', $contains)) {
-           //TODO: Recuperar as reservas que cada área possui.
-        }
+
+        $this->whereResident();
+
+        $this->select([
+            'reservations.*',
+            'areas.name As area',
+        ]);
+
+        $this->join('areas', 'areas.id = reservations.area_id');
+
+        $this->orderBy('reservations.created_at', 'DESC');
+
+        return $this->findAll();
     }
+
+    public function getByCode(string $code, array $contains = []): object
+    {
+        $this->whereResident();
+
+        // Recupera a reserva pelo código
+        $reservation = parent::getByCode(code: $code);
+
+        if ($reservation === null) {
+            throw new \RuntimeException("Reserva com código {$code} não encontrada");
+        }
+
+        // Relaciona dados adicionais, se necessário
+        $this->relateData($reservation, $contains);
+
+        return $reservation;
+    }
+
+
+    protected function relateData(object &$reservation, array $contains = []): void
+    {
+        if (in_array('bill', $contains)) {
+           // TODO: buscar a cobrança associada a essa reserva
+           // $reservation->bill =
+        } 
+
+        if (in_array('resident', $contains)) {           
+            $reservation->resident = model(ResidentModel::class)->where('id', $reservation->resident_id)->first();
+        }
+
+        if (in_array('area', $contains)) {           
+            $reservation->resident = model(AreaModel::class)->where('id', $reservation->area_id)->first();
+        }
+        
+    }
+
+    public function markAs(string $code, Status $status): bool
+    {
+
+        $data =  [
+            'status'        => $status->value,
+            'reason_status' => $status->label(),
+        ];
+
+        return $this->set($data)      
+             ->where('code', $code)
+             ->update();
+    }
+
 }
